3c9251f16b9e01337ee43ce08929d5c9a77351d2
numuhingabiraline-create <numuhingabiraline@gmail.com>
2026-05-14 05:46:56 +0200

    Create Linear search1.cpp

---
+++ Linear search1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[5] = {10, 20, 30, 40, 50};
+    int search, found = -1;
+cout << "Enter number to search: ";
+cin >> search;
+for (int i = 0; i < 5; i++) {
+  if (arr[i] == search) {
+      found = i;
+        break;
+        }
+    }
+if (found != -1)
+cout << "Number found at position " <<
+          found + 1 << endl;
+ else
+cout << "Number not found" << endl;
+return 0;
+}
