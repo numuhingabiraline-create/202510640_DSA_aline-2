fecd2761189d6c271d942b692f0a4ce2ebc009de
numuhingabiraline-create <numuhingabiraline@gmail.com>
2026-05-14 06:24:00 +0200

    Create Binary1.cpp

---
+++ Binary1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[5] = {10, 20, 30, 40, 50};
+    int low = 0, high = 4, mid;
+    int search;
+    bool found = false;
+cout << "Enter number to search: ";
+cin >> search;
+while (low <= high) {
+mid = (low + high) / 2;
+if (arr[mid] == search) {
+cout << "Number found at position " 
+  << mid + 1 << endl;
+    found = true;
+      break;}
+  else if (arr[mid] < search) {
+            low = mid + 1;
+ }
+else {
+  high = mid - 1;
+} }
+if (!found) {
+ cout << "Number not found" << endl;
+    }
+return 0;
+}
